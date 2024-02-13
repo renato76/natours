498c83d676e41ee94885641809d5edea8ded8dbd
Renato Mignogna <renmignogna@gmail.com>
2024-02-13 21:37:23 +0000

    add aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -95,7 +95,12 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start}  milliseconds`)
-  console.log(docs)
+  next()
+})
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } })
   next()
 })
 
