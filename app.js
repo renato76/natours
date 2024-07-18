06227a353b69ed3323f6923bd6cc2bd0caeda5d7
Renato Mignogna <renmignogna@gmail.com>
2024-07-18 21:42:44 +0100

    add rate limit to prevent DOS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -9,10 +10,18 @@ const userRouter = require('./routes/userRoutes')
 const baseUrl = '/api/v1'
 const app = express()
 
-// 1. MIDDLEWARE
+// 1. GLOBAL MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in 1 hour!'
+})
+
+app.use('/api', limiter)
 
 app.use(express.json())
 
