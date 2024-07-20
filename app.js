921d25dcfbb04d38ab075aada9ca6cb561d0d1a9
Renato Mignogna <renmignogna@gmail.com>
2024-07-20 22:05:44 +0100

    add data sanitization packages to use as middleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -11,10 +14,16 @@ const baseUrl = '/api/v1'
 const app = express()
 
 // 1. GLOBAL MIDDLEWARE
+
+// Set security HTTP headers
+app.use(helmet())
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -23,10 +32,19 @@ const limiter = rateLimit({
 
 app.use('/api', limiter)
 
-app.use(express.json())
+// Body parser, reading data from bodyinto req.body
+app.use(express.json({ limit: '10kb' }))
 
+// Data sanatization against NoSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against cross site scripting attacks
+app.use(xss())
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`))
 
+// Test middleware 
 app.use((req, _res, next) => {
   req.requestTime = new Date().toISOString()
   // console.log(req.headers)
