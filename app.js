b3c6db0dc4ee105c07a29d5eaf14a6b2fccfb4ab
Renato Mignogna <renmignogna@gmail.com>
2024-08-17 23:09:51 +0100

    set up project structure

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
+const viewRouter = require('./routes/viewRoutes')
+
 
 const baseUrl = '/api/v1'
 const app = express()
@@ -71,26 +73,8 @@ app.use((req, _res, next) => {
   next()
 })
 
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas'
-  })
-})
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All tours'
-  })
-})
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
-  })
-})
-
 // 2. MOUNTING THE ROUTES
+app.use('/', viewRouter)
 app.use(`${baseUrl}/tours`, tourRouter)
 app.use(`${baseUrl}/users`, userRouter)
 app.use(`${baseUrl}/reviews`, reviewRouter)
