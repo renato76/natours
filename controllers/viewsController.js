e94bebad6082ae85fc2c6320b46ffc363a7b980f
Renato Mignogna <renmignogna@gmail.com>
2024-08-18 22:04:43 +0100

    build the overview template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,20 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel')
+const catchAsync = require('../utils/catchAsync')
+
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1. get tour data
+  const tours = await Tour.find()
+
+  // 2. build template
+
+  // 3. render template with the data
+
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours
   })
-}
+})
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
